173e6c0e6a416b34312bb0beedb560230762b5a8
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2020-03-20 10:42:32 +0330

    Update start text

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,2 +1,10 @@
+
+start_text = """
+سلام! خوش اومدی، راستش این دکمه استارت کار خاصی نمی‌کنه، فقط کمک میکنه که باهات آشنا شم ;)
+اینجا می‌تونی بازی جدید راه بندازی یا به یه بازی ساخته شده اضافه بشی.
+خوش بگذره D:
+"""
+
+
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=start_text)
