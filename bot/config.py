5862987ccb5faf0a0c0ba2e6ad79430ada26c7f1
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2020-03-20 10:02:48 +0330

    Add simple command handler

--- bot/config.py
+++
@@ -1,5 +0,0 @@
-
-TELEGRAM_BOT_TOKEN = "Placeholder"
-WEBHOOK_URL = "http://placeholder"
-KEY = "KEY"
-PORT = 1234
---
+++ bot/main.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater, CommandHandler
+
+from config import TELEGRAM_BOT_TOKEN
+from handlers import start
+
+updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
+
+
